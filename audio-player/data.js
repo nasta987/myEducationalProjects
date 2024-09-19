7b18a7d5ca5021d22099250bf12a04cedaa042db
nasta987 <nasta978@mail.ru>
2024-09-19 21:20:46 +0300

    feat: make the button switch songs. (Thu, 19 Sep 2024 9:20:52 PM)

--- audio-player/data.js
+++ audio-player/data.js
@@ -1,14 +1,14 @@
-let arraySong = [
+let arrayAudio = [
   {
     artist: "Beyonce",
     nameSong: "Dont Hurt Yourself",
     pathSong: "assets/audio/beyonce.mp3",
-    pathBackground: "/assets/image/lemonade.png",
+    pathBackground: "assets/image/lemonade.png",
   },
   {
     artist: "Dua Lipa",
     nameSong: "Dont Start Now",
     pathSong: "assets/audio/dontstartnow.mp3",
-    pathBackground: "/assets/image/dontstartnow.png",
+    pathBackground: "assets/image/dontstartnow.png",
   },
 ];
--- audio-player/index.js
+++ audio-player/index.js
@@ -1,7 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   const audio = document.querySelector("audio");
   const playBtn = document.querySelector(".play-btn");
+  const forwardBtn = document.querySelector(".forward-btn");
+  const backwardBtn = document.querySelector(".backward-btn");
+  const containerAudio = document.querySelector(".container-audio");
+  const audioBackground = document.querySelector(".audio-background");
+  const singer = document.querySelector(".singer");
+  const song = document.querySelector(".song");
 
+  let currentIndex = 0;
+
+  // Включить/выключить песню
   function toggleAudio() {
     if (audio.paused) {
       audio.play();
@@ -13,4 +22,30 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   playBtn.addEventListener("click", toggleAudio);
+
+  // Переключать песни
+  function loadTrack(index) {
+    const track = arrayAudio[index];
+    audio.src = track.pathSong;
+    containerAudio.style.backgroundImage = `url(${track.pathBackground})`;
+    audioBackground.style.backgroundImage = `url(${track.pathBackground})`;
+    singer.textContent = track.artist;
+    song.textContent = track.nameSong;
+    audio.play();
+  }
+
+  function nextTrack() {
+    currentIndex = (currentIndex + 1) % arrayAudio.length;
+    loadTrack(currentIndex);
+  }
+
+  function prevTrack() {
+    currentIndex = (currentIndex - 1 + arrayAudio.length) % arrayAudio.length;
+    loadTrack(currentIndex);
+  }
+  forwardBtn.addEventListener("click", nextTrack);
+  backwardBtn.addEventListener("click", prevTrack);
+
+  // При загрузке страницы - первая песня
+  loadTrack(currentIndex);
 });
