f0390bd5c3b9caab49947559ec697d6550ace696
nasta987 <nasta978@mail.ru>
2024-09-26 20:42:24 +0300

    feat: change index.html (Thu, 26 Sep 2024 8:41:06 PM)

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const url =
-    "https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo";
+  const API_KEY = "SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo";
+  const baseUrl = "https://api.unsplash.com/search/photos";
+  const galleryContainer = document.getElementById("gallery-container");
+  const searchInput = document.getElementById("search-input");
+  const searchBtn = document.getElementById("search-btn");
 
-  async function getData() {
+  // Функция для запроса изображений
+  async function fetchImages(query = "spring") {
+    const url = `${baseUrl}?query=${query}&per_page=30&orientation=landscape&client_id=${API_KEY}`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+    return data.results; // Возвращаем список изображений
   }
-  getData();
 });
