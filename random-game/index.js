6b5e3f0a4e661f80f0823ab4216269d815a66ff1
nasta987 <nasta978@mail.ru>
2024-10-09 19:59:51 +0300

    feat: add dynamic cards to the page. (Wed, 09 Oct 2024 9:59:23 PM)

--- random-game/index.js
+++ random-game/index.js
@@ -14,4 +14,22 @@ document.addEventListener("DOMContentLoaded", function () {
     `;
     memoryCards.appendChild(card);
   });
+
+  // Переворот карточек
+  const cardElements = document.querySelectorAll(".memory-card");
+
+  cardElements.forEach((card) => {
+    card.addEventListener("click", () => {
+      card.classList.toggle("flip");
+    });
+  });
+
+  // Переворот карточек рубашкой вверх через три секунды после загрузки страницы
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      cardElements.forEach((card) => {
+        card.classList.add("flip"); // Переворот каждой карточки
+      });
+    }, 4000); // 3 секунды
+  });
 });
